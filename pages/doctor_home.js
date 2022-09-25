65dffcabc043c58bf1f0def793f4552aa4aa9f04
IbrahimGhasia <ibrahim.ghasia@gmail.com>
2022-09-25 17:52:44 +0530

    Fixed the link error in doctor home

--- pages/doctor_home.js
+++ pages/doctor_home.js
@@ -106,7 +106,7 @@ export default function Home() {
                                     You have not registed before! Please register first.
                                 </span>
 
-                                <Link href="/patient_edit">
+                                <Link href="/doctor_edit">
                                     <div className="text-center">
                                         <button className="mt-2 text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-10 py-2.5 text-center mr-2 mb-2">
                                             Register Now
